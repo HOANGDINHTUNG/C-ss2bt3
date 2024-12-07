e4f655cf3cd1b95bc6808282f296ada2c9e19fb4
HOANGDINHTUNG <mtung3365@gmail.com>
2024-12-07 20:31:12 +0700

    Add files via upload

---
+++ bt3ss2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int a = 10;
+	int b = 100;
+	int tong = a + b;
+	int thuong = b / a;
+	int hieu = b - a;
+	int tich = a * b;
+	printf("tong= %d\n", tong);
+	printf("hieu= %d\n", hieu);
+	printf("tich= %d\n", tich);
+	printf("thuong= %d\n", thuong);
+}
+
